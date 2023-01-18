51db34e483b0e336e1c5b96f749a8e581b94cfcf
Samuel Martins Francisco <96782173+SamuelMartinsFrancisco@users.noreply.github.com>
2023-01-18 10:04:30 -0300

    Update AboutModal.jsx

--- src/components/AboutModal.jsx
+++ src/components/AboutModal.jsx
@@ -4,7 +4,7 @@ import './AboutModal.css';
 function AboutModal(props) {
     return (
         <div className={`modal ${props.show}`}>
-            <h2> <a href="https://github.com/SamuelMartinsFrancisco/Github-Finder/" target="_blank" rel="noreferrer"> Github Finder </a> é um buscador de usuários e repositórios presentes no GitHub.</h2>
+            <h2> <a href="https://github.com/SamuelMartinsFrancisco/Github-Finder/" target="_blank" rel="noreferrer"> Github Finder </a> é um buscador de usuários e repositórios presentes no <a href="https://pt.wikipedia.org/wiki/GitHub" target="_blank" rel="noreferrer">GitHub</a>.</h2>
         </div>
     );
 }
